5ea3b75905c1b84832cdb78f9df82bdf2ada8d29
programworld999 <42247997+programworld999@users.noreply.github.com>
2020-03-24 03:08:58 +0530

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -1,47 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[114]:
-
-
 import os
 import requests
 from bs4 import BeautifulSoup as BS
 import json
 
-
-# In[115]:
-
-
 url = "https://ncov.dxy.cn/ncovh5/view/en_pneumonia?from=dxy&source=&link=&share="
 page = requests.get(url)
-
-
-# In[116]:
-
 
 soup = BS(page.content, 'html.parser')
 ByCountry = soup.find('script', attrs = {'id':'getListByCountryTypeService2true'})
 
-
-# In[117]:
-
-
 ByCountry = ByCountry.text[48:]
 ByCountry = ByCountry[:-11]
-
-
-# In[118]:
-
 
 ByCountry = str(ByCountry)
 ByCountry = ByCountry.replace("Syrian\xa0ArabRepublic", "Syrian Arab Republic")
 ByCountry = ByCountry.replace("'", '"')
 ByCountry = json.loads(ByCountry)
-
-
-# In[119]:
-
 
 globalData = soup.find('script', attrs = {'id':'getStatisticsService'})
 
@@ -49,48 +26,22 @@ globalData = globalData.text[36:]
 globalData = globalData[:-11]
 globalData = json.loads(str(globalData))
 
-
-# In[136]:
-
-
 globalStatistics = globalData["globalStatistics"]
-print("World Wide: Confirmed: ", globalStatistics['confirmedCount'], "| Active: ", globalStatistics['currentConfirmedCount'], "| Death: ", globalStatistics['deadCount'], "| Recovered: ", globalStatistics['curedCount'])
-
-
-# In[137]:
-
+print("\033[1mWorld Wide: Confirmed: ", globalStatistics['confirmedCount'], "| Active: ", globalStatistics['currentConfirmedCount'], "| Death: ", globalStatistics['deadCount'], "| Recovered: ", globalStatistics['curedCount'], "\033[0m \n")
 
 for ByCountrys in ByCountry:
-    print(ByCountrys["countryFullName"], ": Confirmed: ", ByCountrys['confirmedCount'], "| Active: ", ByCountrys['currentConfirmedCount'], "| Death: ", ByCountrys['deadCount'], "| Recovered: ", ByCountrys['curedCount'])
-
-
-# In[142]:
-
-
+    print(ByCountrys["countryFullName"], ": \033[1;33;40m Confirmed: ", ByCountrys['confirmedCount'], "\033[0m|\033[1;35;40m Active: ", ByCountrys['currentConfirmedCount'], "\033[0m|\033[0;37;41m Death: ", ByCountrys['deadCount'], "\033[0m|\033[1;32;40m Recovered: ", ByCountrys['curedCount'], "\033[0m")
+    
 def searchByCountry(country):
     for ByCountrys in ByCountry:
         if(ByCountrys["countryFullName"] == country):
-            print(ByCountrys["countryFullName"], ": Confirmed: ", ByCountrys['confirmedCount'], "| Active: ", ByCountrys['currentConfirmedCount'], "| Death: ", ByCountrys['deadCount'], "| Recovered: ", ByCountrys['curedCount'])
+            print(ByCountrys["countryFullName"], ": \033[1;33;40m Confirmed: ", ByCountrys['confirmedCount'], "\033[0m|\033[1;35;40m Active: ", ByCountrys['currentConfirmedCount'], "\033[0m|\033[0;37;41m Death: ", ByCountrys['deadCount'], "\033[0m|\033[1;32;40m Recovered: ", ByCountrys['curedCount'], "\033[0m")
             return 0
     print("Country Name Wrong!")
+    
+while(1):
+  searchByCountry(input("Country Name: "))
 
 
-# In[151]:
 
-
-searchByCountry(input("Country Name: "))
-
-
-# In[147]:
-
-
-ByCountry[0]
-
-
-# In[149]:
-
-
-from IPython.display import Image
-imgsrc = "https://img1.dxycdn.com/2020/0319/685/3402856849412997239-135.png"
-Image(filename='example.png')
 
